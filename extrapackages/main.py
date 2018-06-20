83d5bd9068f436136fdbf82b0537d1c0dfc8f18f
Raul Rodrigo Segura <raurodse@gmail.com>
2018-06-20 18:31:17 +0200

    refactory code

--- extrapackages/main.py
+++ extrapackages/main.py
@@ -13,6 +13,12 @@ from PythonQt.QtCore import *
 from PythonQt.QtSvg import QSvgWidget
 import PythonQt.calamares as calamares
 
+# Set up translations.
+# You may skip this if your Calamares module has no user visible strings.
+# DO NOT install _ into the builtin namespace because each module loads
+# its own catalog.
+# DO use the gettext class-based API and manually alias _ as described in:
+# https://docs.python.org/3.5/library/gettext.html#localizing-your-module
 import gettext
 import inspect
 import os
@@ -21,15 +27,40 @@ _path = os.path.dirname(os.path.abspath(_filename))
 
 _ = gettext.gettext
 
+# Example Python ViewModule.
+# A Python ViewModule is a Python program which defines a ViewStep class.
+# One UI module ==> one ViewStep.
+# This class must be marked with the @calamares_module decorator. A
+# ViewModule may define other classes, but only one may be decorated with
+# @calamares_module. Such a class must conform to the Calamares ViewStep
+# interface and functions as the entry point of the module.
+# A ViewStep manages one or more "wizard pages" through methods like
+# back/next, and reports its status through isNextEnabled/isBackEnabled/
+# isAtBeginning/isAtEnd. The whole UI, including all the pages, must be
+# exposed as a single QWidget, returned by the widget function.
+#
+# For convenience, both C++ and PythonQt ViewSteps are considered to be
+# implementations of ViewStep.h. Additionally, the Calamares PythonQt API
+# allows Python developers to keep their identifiers more Pythonic on the
+# Python side. Thus, all of the following are considered valid method
+# identifiers in a ViewStep implementation: isNextEnabled, isnextenabled,
+# is_next_enabled.
+
+
 @calamares_module
 class ExtraPackagesViewStep:
     def __init__(self):
 
+        initial_configuration = calamares.global_storage.value('packageOperations')
+        if initial_configuration is None:
+            calamares.global_storage.insert('packageOperations',({'try_install':()},))
         self.main_widget = QFrame()
         self.main_widget.setLayout(QVBoxLayout())
         qsa = QScrollArea()
         widget = QWidget()
         widget.setLayout(QVBoxLayout())
+        print(calamares.global_storage.keys())
+        widget.layout().setAlignment(Qt.AlignCenter | Qt.AlignTop)
         qsa.setWidget(widget)
         qsa.setWidgetResizable(True)
 
@@ -45,21 +76,18 @@ class ExtraPackagesViewStep:
         label.setFont(fontlabel)
         label.setStyleSheet("QLabel{ margin:0px 0px 10px 0px}")
 
+        #btn = QPushButton()
+        count = 0
         for app in calamares.configuration['packages']:
-            print(app)
             config = calamares.configuration['packages'][app]
-            print(config['name'])
-            print(config['description'])
-            print(config['image'])
-            print(config['checked'])
-            print(config['package'])
-            widget.layout().addLayout(self.add_package(config))
+            last = False
+            if count+1== len(calamares.configuration['packages']):
+                last = True
+            widget.layout().addLayout(self.newPackageUI(config), last)
+            count+=1
 
 
-    def add_package(self, config, last=False):
-        vLayout = QVBoxLayout()
-        horizontalLayout = QHBoxLayout()
-        horizontalLayout.setObjectName("horizontalLayout")
+    def createImagePackage(self,config):
         is_img = True
         if imghdr.what(config['image']) == '':
             is_img = False
@@ -68,7 +96,6 @@ class ExtraPackagesViewStep:
             label.setText("")
             label.setScaledContents(True)
             label.setPixmap(QPixmap(config['image']))
-
         else:
             label = QSvgWidget()
             label.load(config['image'])
@@ -77,16 +104,10 @@ class ExtraPackagesViewStep:
         sizePolicy.setVerticalStretch(0)
         label.setSizePolicy(sizePolicy)
         label.setMaximumSize(QSize(40, 40))
-        label.setObjectName("label")
+        label.setObjectName("imagePackage")
+        return label
 
-
-        horizontalLayout.addWidget(label)
-        verticalLayout_3 = QVBoxLayout()
-        verticalLayout_3.setObjectName("verticalLayout_3")
-        horizontalLayout_2 = QHBoxLayout()
-        horizontalLayout_2.setObjectName("horizontalLayout_2")
-        verticalLayout_4 = QVBoxLayout()
-        verticalLayout_4.setObjectName("verticalLayout_4")
+    def createNamePackage(self,config):
         label_3 = QLabel()
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -98,16 +119,22 @@ class ExtraPackagesViewStep:
         label_3.setFont(font)
         label_3.setObjectName("label_3")
         label_3.setStyleSheet("QLabel{margin-left:5px; }")
-        verticalLayout_4.addWidget(label_3)
+        label_3.setText(_(config['name']))
+        return label_3
+
+    def createDescriptionPackage(self,config):
         label_2 = QLabel()
+        label_2.setWordWrap(True)
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         label_2.setSizePolicy(sizePolicy)
         label_2.setObjectName("label_2")
         label_2.setStyleSheet("QLabel{margin-left:5px ; color: #666 }")
-        verticalLayout_4.addWidget(label_2)
-        horizontalLayout_2.addLayout(verticalLayout_4)
+        label_2.setText(_(config['description']))
+        return label_2
+
+    def createCheckInstallPackage(self,config):
         checkBox = QCheckBox()
         sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -116,15 +143,39 @@ class ExtraPackagesViewStep:
         checkBox.setObjectName("checkBox")
         checkBox.setChecked(config['checked'])
         checkBox.connect("clicked(bool)",lambda: self.modify_package(config['package'],checkBox))
-        horizontalLayout_2.addWidget(checkBox)
+        if config['checked']:
+            self.modify_package(config['package'],checkBox)
+        checkBox.setText(_("Install"))
+        return checkBox
+
+
+    def newPackageUI(self, config, last=False):
+        vLayout = QVBoxLayout()
+        horizontalLayout = QHBoxLayout()
+        horizontalLayout.setObjectName("horizontalLayout")
+        verticalLayout_3 = QVBoxLayout()
+        verticalLayout_3.setObjectName("verticalLayout_3")
+        horizontalLayout_2 = QHBoxLayout()
+        horizontalLayout_2.setObjectName("horizontalLayout_2")
+        verticalLayout_4 = QVBoxLayout()
+        verticalLayout_4.setObjectName("verticalLayout_4")
+        horizontalLayout_2.addLayout(verticalLayout_4)
+        
+        image_package = self.createImagePackage(config)
+        name_package = self.createNamePackage(config)
+        description_package = self.createDescriptionPackage(config)
+        install_package = self.createCheckInstallPackage(config)
+
+        horizontalLayout.addWidget(image_package)
+        verticalLayout_4.addWidget(name_package)
+        verticalLayout_4.addWidget(description_package)
+        horizontalLayout_2.addWidget(install_package)
+
         verticalLayout_3.addLayout(horizontalLayout_2)
         horizontalLayout.addLayout(verticalLayout_3)
         vLayout.addLayout(horizontalLayout)
         if not last:
             verticalLayout_3.addWidget(self.add_line())
-        label_3.setText(_(config['name']))
-        label_2.setText(_(config['description']))
-        checkBox.setText(_("Install"))
         return vLayout
 
     def add_line(self):
@@ -137,8 +188,33 @@ class ExtraPackagesViewStep:
         return line
 
     def modify_package(self, package_name, checkbox):
-        print(package_name,checkbox.isChecked())
+        all_packages = calamares.global_storage.value('packageOperations')
+        if checkbox.isChecked():
+            all_packages = self.install_package(all_packages, package_name)
+        else:
+            all_packages = self.remove_install_package(all_packages, package_name)
+        calamares.global_storage.insert('packageOperations',all_packages)
 
+    def install_package(self, list_packages, package):
+        found = False
+        for list_index in range(0,len(list_packages)):
+            install_keys = [ x for x in ['install','try_install'] if x in list_packages[list_index] ]
+            for x in install_keys:
+                if package in list_packages[list_index][x]:
+                    found = True
+        if not found:
+            if 'try_install' not in list_packages[0]:
+                list_packages[0] = {'try_install':()}
+            list_packages[0]['try_install'] = list_packages[0]['try_install'] + (package,)
+        return list_packages
+
+    def remove_install_package(self,list_packages, package):
+        for list_index in range(0,len(list_packages)):
+            if 'try_install' not in list_packages[list_index]:
+                continue
+            if package in list_packages[list_index]['try_install']:
+                list_packages[list_index]['try_install'] = tuple(pack_elem for pack_elem in list_packages[list_index]['try_install'] if pack_elem != package)
+        return list_packages
 
     def prettyName(self):
         return _("Dummy PythonQt ViewStep")
